b264704157b07243cadc608a4771c913a3f31151
jleetaylor2020 <jleetaylor2020@icloud.com>
2018-11-07 11:59:37 -0600

    changes to operators

--- app5.js
+++ app5.js
@@ -1,27 +1,29 @@
 let cars = ['Mustang', 'Porsche', 'Mercedes'];
 let title = 'Awesome Cars';
+let numbers = [2, 5, 10];
 
-
+// CARS EXAMPLE//
 // shows the index list of array in console debugger
 console.log (cars);
 console.log (title);
-
 // 3
 console.log(cars.length);
-
 // add function
 console.log (typeof title.toUpperCase);
-
 // Loop over an Array: this lists the cars items in order AND lists indexes
     cars.forEach(function(item, index, array) {
     console.log(item, index);
     
 });
-
 // Add to the end of an array - adds Chevy to end of list, length changes from 3 to 4
 let newLength = cars.push('Chevy');
-
-// SHIFT - removes 'Mustang' from the front of an array
+// SHIFT - removes item ie. 'Mustang' from the front of an array
 cars.shift();
+// UNSHIFT - adds item ie 'Ford' to the front of any array
+cars.unshift ('Ford');
+// remove an item ie 'Porsche' by its index position from the array ie "Chevy" ws removed.
+let removedItem = cars.splice ([3]);
 
-// UNSHIFT - 
+//NUMBERS EXAMPLE
+// indexes the item numbers [2, 5, 10]
+console.log (numbers);
---
+++ app6.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+// Operand and Operator - operators are applied to operand. For example:
+// ..Expression  8 * 3  there are two operands. Left operand 8, Right oeprand 3
+// ..Operands can be referred to as ARGUMENTS.
+
+// JavaScript has the following types of operators:....
+// assignment operators
+// comparison operators
+// arithmetic operators
+// bitwise operators
+// logical operators
+// string operators
+// conditional (or ternary) operators
+// comma operator
+// unary operator
+// relational operators
